2644458e9d29f0d475a71f8735df8579a9072ae9
huseyingokay <huseyin.gokay@ozu.edu.tr>
2021-07-09 22:02:05 +0300

    zipped projects are implemented

--- clone-project.sh
+++ clone-project.sh
@@ -3,15 +3,22 @@ slug=$1
 sha=$2
 cd ~/
 
-if [[ ! -d "$slug" ]]; then
+if [[ ! -f "/input/$slug.zip" ]]; then
     echo "================Cloning the project: $(date)"
+    cd input
     git clone https://github.com/$slug $slug
+    zip $slug
+    cp $slug.zip $AZ_BATCH_TASK_WORKING_DIR
+    unzip $slug.zip
     cd $slug
     git checkout $sha
     echo "SHA is $(git rev-parse HEAD)"
 else
+    cp /input/$slug.zip .
+    unzip $slug.zip
     cd $slug
-    echo "$slug already cloned"
+    git checkout $sha
+    echo "$slug already exists"
     echo "SHA is $(git rev-parse HEAD)"
 fi
 if [[ "$(git rev-parse HEAD)" == "$sha" ]]; then

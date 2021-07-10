36dda2fefcddb71b5b308214f012189d2ef679b2
huseyingokay <huseyin.gokay@ozu.edu.tr>
2021-07-10 23:38:34 +0300

    bug fix4

--- clone-project.sh
+++ clone-project.sh
@@ -1,4 +1,3 @@
-
 slug=$1
 sha=$2
 projectname=${slug%/*}
@@ -18,7 +17,7 @@ if [[ ! -f "$AZ_BATCH_TASK_WORKING_DIR/input/$projectname.zip" ]]; then
     git checkout $sha
     echo "SHA is $(git rev-parse HEAD)"
 else
-    cp /input/$projectname.zip .
+    cp $AZ_BATCH_TASK_WORKING_DIR/input/$projectname.zip .
     unzip $projectname.zip
     cd $slug
     git checkout $sha

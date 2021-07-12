5d2fd186352c20e2619801d4b6f72f2f25c44bbb
huseyingokay <huseyin.gokay@ozu.edu.tr>
2021-07-12 22:09:03 +0300

    changes made in run_surefire are removed

--- clone-project.sh
+++ clone-project.sh
@@ -1,19 +1,21 @@
 slug=$1
 sha=$2
-input_container=$3
 projectname=${slug%/*}
+
+echo "in clone-project.sh"
+echo "slug: $slug"
+echo "sha: $sha"
+echo "projectname: $projectname"
+
 cd ~/
 
-if [[ ! -f "$AZ_BATCH_TASK_WORKING_DIR/$input_container/$projectname.zip" ]]; then
-    echo "================Cloning the project: $(date)"
-    echo "================Input: $input_container"
-    cd $input_container
+if [[ ! -f "$AZ_BATCH_TASK_WORKING_DIR/input/$projectname.zip" ]]; then
     git clone https://github.com/$slug $slug
     cd $AZ_BATCH_TASK_WORKING_DIR/$input_container/$slug
     git checkout $sha
     echo "SHA is $(git rev-parse HEAD)"
 else
-    cp $AZ_BATCH_TASK_WORKING_DIR/$input_container/$projectname.zip .
+    cp $AZ_BATCH_TASK_WORKING_DIR/input/$projectname.zip .
     unzip $projectname.zip
     cd $slug
     git checkout $sha
--- run_surefire_rule_azure.sh
+++ run_surefire_rule_azure.sh
@@ -18,32 +18,25 @@ mkdir -p ${RESULTSDIR}
 cd ~/
 projfile=$1
 rounds=$2
-input_container=$3
-
 line=$(head -n 1 $projfile)
 
 echo "================Starting experiment for input: $line"
 slug=$(echo ${line} | cut -d',' -f1 | rev | cut -d'/' -f1-2 | rev)
 sha=$(echo ${line} | cut -d',' -f2)
-projectname=${slug%/*}
+
 fullTestName="running.idempotent"
 module=$(echo ${line} | cut -d',' -f3)
 
-MVNOPTIONS="-Ddependency-check.skip=true -Dmaven.repo.local=$AZ_BATCH_TASK_WORKING_DIR/$input_container/dependencies -Dgpg.skip=true -DfailIfNoTests=false -Dskip.installnodenpm -Dskip.npm -Dskip.yarn -Dlicense.skip -Dcheckstyle.skip -Drat.skip -Denforcer.skip -Danimal.sniffer.skip -Dmaven.javadoc.skip -Dfindbugs.skip -Dwarbucks.skip -Dmodernizer.skip -Dimpsort.skip -Dmdep.analyze.skip -Dpgpverify.skip -Dxml.skip -Dcobertura.skip=true -Dfindbugs.skip=true"
+MVNOPTIONS="-Ddependency-check.skip=true -Dgpg.skip=true -DfailIfNoTests=false -Dskip.installnodenpm -Dskip.npm -Dskip.yarn -Dlicense.skip -Dcheckstyle.skip -Drat.skip -Denforcer.skip -Danimal.sniffer.skip -Dmaven.javadoc.skip -Dfindbugs.skip -Dwarbucks.skip -Dmodernizer.skip -Dimpsort.skip -Dmdep.analyze.skip -Dpgpverify.skip -Dxml.skip -Dcobertura.skip=true -Dfindbugs.skip=true"
 
 modifiedslug=$(echo ${slug} | sed 's;/;.;' | tr '[:upper:]' '[:lower:]')
 short_sha=${sha:0:7}
 modifiedslug_with_sha="${modifiedslug}-${short_sha}"
 
 # echo "================Cloning the project"
-bash $dir/clone-project.sh "$slug" "$sha" "$input_container"
+bash $dir/clone-project.sh "$slug" "$sha"
+cd ~/$slug
 
-currentdir=$slug
-if [[ -d "$AZ_BATCH_TASK_WORKING_DIR/$input_container/$projectname" ]]; then
-    currentdir=$input_container/$slug
-fi
-
-cd ~/$currentdir
 echo "================Setting up test name: $(date)"
 testarg=""
 if [[ $fullTestName == "-" ]] || [[ "$fullTestName" == "" ]]; then
@@ -61,27 +54,25 @@ fi
 if [[ -z $module ]]; then
     module=$classloc
     while [[ "$module" != "." && "$module" != "" ]]; do
-    module=$(echo $module | rev | cut -d'/' -f2- | rev)
-    echo "Checking for pom at: $module"
-    if [[ -f $module/pom.xml ]]; then
-        break;
-    fi
+	module=$(echo $module | rev | cut -d'/' -f2- | rev)
+	echo "Checking for pom at: $module"
+	if [[ -f $module/pom.xml ]]; then
+	    break;
+	fi
     done
 else
     echo "Module passed in from csv."
 fi
 echo "Location of module: $module"
 
-if [[ -d "$AZ_BATCH_TASK_WORKING_DIR/$input_container/$projectname" ]]; then
-    # echo "================Installing the project"
-    bash $dir/install-project.sh "$slug" "$MVNOPTIONS" "$USER" "$module" "$sha" "$dir" "$fullTestName" "${RESULTSDIR}" "$input_container"
-    ret=${PIPESTATUS[0]}
-    mv mvn-install.log ${RESULTSDIR}
-    if [[ $ret != 0 ]]; then
-        # mvn install does not compile - return 0
-        echo "Compilation failed. Actual: $ret"
-        exit 1
-    fi
+# echo "================Installing the project"
+bash $dir/install-project.sh "$slug" "$MVNOPTIONS" "$USER" "$module" "$sha" "$dir" "$fullTestName" "${RESULTSDIR}"
+ret=${PIPESTATUS[0]}
+mv mvn-install.log ${RESULTSDIR}
+if [[ $ret != 0 ]]; then
+    # mvn install does not compile - return 0
+    echo "Compilation failed. Actual: $ret"
+    exit 1
 fi
 
 cd ~/$slug

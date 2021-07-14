0c144bff74bce3d17e08b967351c72c283287c1b
huseyingokay <huseyin.gokay@ozu.edu.tr>
2021-07-14 13:25:47 +0300

    report feature added4

--- compile-project.sh
+++ compile-project.sh
@@ -54,7 +54,8 @@ echo "Location of module: $module"
 echo "================Compiling: $(date)"
 mvn compile --log-file=$AZ_BATCH_TASK_WORKING_DIR/"com=${modifiedslug_with_sha}=${modified_module}".txt
 
-if grep -Fxq "BUILD SUCCESS" "com=$modifiedslug_with_sha=$modified_module".txt then
+if grep -Fxq "BUILD SUCCESS" "com=$modifiedslug_with_sha=$modified_module".txt 
+then
     echo "com=${modifiedslug_with_sha}=${modified_module} is compiled successfully." | tee –a /$AZ_BATCH_TASK_WORKING_DIR/$input_container/results.txt
 else
     echo "com=${modifiedslug_with_sha}=${modified_module} is failed." | tee –a $AZ_BATCH_TASK_WORKING_DIR/$input_container/results.txt
